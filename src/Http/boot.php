ef605c1fc216ada8d56b3cea624e70987b158217
Jérémy Munsch <github@jeremydev.ovh>
2022-01-20 10:39:56 +0100

    #39 FIX mb http input

--- src/Http/boot.php
+++ src/Http/boot.php
@@ -19,7 +19,7 @@ use \Kwaadpepper\ResponsiveFileManager\RFM;
 
 mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
-mb_http_input('UTF-8');
+mb_http_input(null);
 mb_language('uni');
 mb_regex_encoding('UTF-8');
 // ob_start('mb_output_handler');
